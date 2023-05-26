8b643345b0394565c3ca557ae0017f074ec0e25a
AP Mobile Store <xtermsoftech@gmail.com>
2023-05-26 12:58:27 +0530

    PWA_SSL
    PWA_SSL

---
+++ index.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="hi-IN" prefix="og: https://ogp.me/ns#">
+<!-- head-->
+<head>
+<!-- Meta Tags Start-->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <title>Under News</title>
+	<meta name="keywords" content="Under News, Freelance, Media, News, Latest News, Local News, Indian News, India News, Old News,  Breking News, Fresh News, News Media, Elections News, Poltics News, Oponion News, Entertainment News, Investigations, Lifestyle, Tech News, Video, Sports, Audio, Education, Premium, Crime, Crime News, World News, Saharanpur News, Deoband News, Muzaffarnagar News, Roorkee News, Haridwar News, Rishikesh News, Dehradun News, Ambala News, Kurukshetra News, Karnal News, Panipat News, Bagpat News, Shamli News, Deoband News, Gangoh News, Nagal News, Behat News">
+	<meta name="description" content="Number One website in the field of news media.">
+	<meta name="author" content="Manoj Kumar">
+	<meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
+	<link rel="canonical" href="https://vaidikgurukuldalheri.in/index.php" />
+	<link rel="next" href="https://vaidikgurukuldalheri.in/index.php" />
+	<meta property="og:title" content="Under News: न्यूज मीडिया के क्षेत्र की नंबर वन वेबसाइट | अपने आस पास के क्षेत्र, देश, विदेश की सभी खबरों के लिए विज़िट करें. " />
+	<meta property="og:description" content="Number One website in the field of news media." />
+	<meta property="og:locale" content="hi-IN" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://vaidikgurukuldalheri.in/" />
+	<meta property="og:site_name" content="Under News" />
+	<meta property="og:image" content="https://vaidikgurukuldalheri.in/icons/logo.png" />
+	<meta property="og:image:secure_url" content="https://vaidikgurukuldalheri.ini/cons/logo.png" />
+	<meta property="og:image:width" content="512" />
+	<meta property="og:image:height" content="512" />
+	<meta property="og:image:alt" content="Under News main Logo" />
+	<meta property="og:image:type" content="image/png" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Under News: न्यूज मीडिया के क्षेत्र की नंबर वन वेबसाइट | अपने आस पास के क्षेत्र, देश , विदेश की सभी खबरों के लिए विज़िट करें. " />
+	<meta name="twitter:description" content="Number One website in the field of news media." />
+	<meta name="twitter:image" content="https://vaidikgurukuldalheri.in/icons/logo.png" />
+	<meta name="twitter:image:src" content="https://vaidikgurukuldalheri.in/icons/logo.png">
+	<!--Meta Tag End-->
+	
+	<!-- ****** favicons Start ****** -->
+	<link rel="shortcut icon" href="icons/favicon.ico">
+	<link rel="icon" sizes="16x16 32x32 64x64" href="icons/favicon.ico">
+	<!-- ****** favicons End ****** -->
+	
+	<!-- ****** PWA Start ****** -->
+	<meta name="msapplication-TileColor" content="#2d89ef">
+	<meta name="msapplication-TileImage" content="icons/android/android-launchericon-144-144.png">
+	<meta name="msapplication-tap-highlight" content="no">
+	<meta name="apple-mobile-web-app-title" content="Under News">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="white">
+    <meta name="application-name" content="Under News">
+	<meta name="theme-color" media="(prefers-color-scheme: light)" content="#FFCC00" />
+	<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000000" />
+	<script src="icons/app.js"></script>
+	<link rel="manifest" href="icons/manifest.json">
+    <link rel="mask-icon" href="icons/safari-pinned-tab.svg?v=1" color="#5bbad5">
+	<!-- ****** PWA End ****** -->
+</head>
+
+<body>
+hello
+</body>
+</html>
